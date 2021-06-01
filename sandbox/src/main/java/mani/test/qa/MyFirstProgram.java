327b6fec3db16a62c4ac1293c9c25a63bce597b8
xomyag <savraska.n@gmail.com>
2021-06-01 22:18:04 +0300

    111

--- sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
+++ sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
@@ -3,7 +3,7 @@ package mani.test.qa;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-System.out.println("Hello, world!");
+System.out.println("Hello, world_111!");
 	}
 
 }
