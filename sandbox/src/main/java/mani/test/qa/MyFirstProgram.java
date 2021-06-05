4879ec277ec4359f76d69e4270a08fb98841ed6b
xomyag <savraska.n@gmail.com>
2021-06-05 18:00:49 +0300

    create some methods

--- sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
+++ sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
@@ -7,21 +7,17 @@ public class MyFirstProgram {
 		hello("456");
 
 		Square s = new Square(5);
-		System.out.println("здесь происходит вывод функции КЕК (кек в квадрате, кек равен " + s.l + ") ,а расчёт функции равен " + area(s));
+		System.out.println("здесь происходит вывод функции КЕК (кек в квадрате, кек равен " + s.l + ") ,а расчёт функции равен " + s.area());
 
 		Rectangle r = new Rectangle(4, 5);
-		System.out.println("а здесь мы закидываем умножение двух интов. А равен \"" + r.a + "\", а B равен \"" + r.b + "\". и само умножение равно " +area(r));
+		System.out.println("а здесь мы закидываем умножение двух интов. А равен \"" + r.a + "\", а B равен \"" + r.b + "\". и само умножение равно " + r.area());
 	}
 
 	public static void hello(String lol) {
 		System.out.println("Hello, "+ lol +"!");
 	}
 
-	public static double area(Square s){
-		return s.l * s.l;
-	}
-	public static double area(Rectangle r){
-		return r.a * r.b;
-	}
+
+
 
 }
