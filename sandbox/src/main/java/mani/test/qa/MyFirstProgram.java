c434c2f905f35be18c81f9d3d3b0cd5265e6abd3
xomyag <savraska.n@gmail.com>
2021-08-10 23:31:01 +0300

    Delete Coordinate class

--- sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
+++ sandbox/src/main/java/mani/test/qa/MyFirstProgram.java
@@ -7,7 +7,7 @@ public class MyFirstProgram {
 		hello("456");
 
 		Square s = new Square(5);
-		System.out.println("здесь происходит вывод функции КЕК (кек в квадрате, кек равен " + s.l + ") ,а расчёт функции равен " + s.area());
+		System.out.println("здесь происходит вывод функци1и КЕК (кек в квадрате, кек равен " + s.l + ") ,а расчёт функции равен " + s.area());
 
 		Rectangle r = new Rectangle(4, 5);
 		System.out.println("а здесь мы закидываем умножение двух интов. А равен \"" + r.a + "\", а B равен \"" + r.b + "\". и само умножение равно " + r.area());
